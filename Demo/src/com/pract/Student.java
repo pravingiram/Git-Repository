5d89874780d31f18834a0a9c022ceb8ba8a595f5
pravingiram <pravingiram7@gmail.com>
2023-04-24 15:19:44 +0530

    student constructor created

---
+++ Demo/src/com/pract/Student.java
@@ -0,0 +1,14 @@
+package com.pract;
+
+public class Student {
+
+	
+	public Student(int a,String name) {
+		System.out.println(a+"  " +name);
+		
+	}
+	public static void main(String[] args) {
+		Student student=new Student(10, "Pravin");
+		
+	}
+}
